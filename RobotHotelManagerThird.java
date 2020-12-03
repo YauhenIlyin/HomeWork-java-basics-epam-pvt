8871ba78013f396d06271c00ace0508acb7330ab
YauhenIlyin <prog.jekylin@gmail.com>
2020-12-03 10:12:47 +0300

    HomeWork1 3/12/2020

---
+++ RobotHotelManagerThird.java
@@ -0,0 +1,21 @@
+package HomeWork1;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class RobotHotelManagerThird {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, i am Freeman, hotel manager. You are welcome! What is your name?");
+        String guestName = sc.nextLine();
+        switch(guestName){
+            case "Вася": System.out.println("Привет!\nЯ тебя так долго ждал");
+            break;
+            case "Анастасия": System.out.println("Я тебя так долго ждал");
+            break;
+            default: System.out.println("Добрый день, а вы кто?");
+        }
+        sc.close();
+    }
+}
